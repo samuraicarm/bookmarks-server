679d6ce90d18eee76a55cd53b6dbb4f8a9db33c6
cd <carmendukes@gmail.com>
2020-10-30 20:50:26 -0700

    fixes cors

--- src/config.js
+++ src/config.js
@@ -1,5 +1,9 @@
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/bookmarks',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/bookmarks'
-}
+  PORT: process.env.PORT || 8000,
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://dunder_mifflin@localhost/bookmarks",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://dunder_mifflin@localhost/bookmarks",
+};
